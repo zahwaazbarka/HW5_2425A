49bd3dfc8e01e3e30d28d622b29a2be845de1d7b
EgbariaMohammad <agbariya@campus.technion.ac.il>
2025-01-28 03:18:56 +0200

    Upload HW5 Files

---
+++ enigma.py
@@ -0,0 +1,11 @@
+
+class Enigma:
+    def __init__(self, hash_map, wheels, reflector_map):
+        pass
+
+    def encrypt(self, message):
+        pass
+
+
+def load_enigma_from_path(path):
+    pass
